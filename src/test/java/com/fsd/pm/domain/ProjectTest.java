e6fa3f88fb24064f95ba062a27ea86e0ee320afc
Jinu Vijay <jinu.vijay@gmail.com>
2018-11-27 21:57:45 -0500

    Initial test cases

---
+++ src/test/java/com/fsd/pm/domain/ProjectTest.java
@@ -0,0 +1,33 @@
+package com.fsd.pm.domain;
+
+import java.util.Date;
+
+import org.hamcrest.core.IsEqual;
+import org.junit.Test;
+
+import static org.junit.Assert.assertThat;
+
+public class ProjectTest {
+
+	@Test
+	public void test() {
+		Project testObj = new Project();
+
+		Date sampleDate = new Date();
+		testObj.setPriority(1);
+		testObj.setProject("Project Name");
+		testObj.setEndDate(sampleDate);
+		testObj.setProjectId(1);
+		testObj.setStartDate(sampleDate);
+		testObj.setTasks(null);
+		
+		assertThat(testObj.getPriority(), IsEqual.equalTo(1));
+		assertThat(testObj.getProject(), IsEqual.equalTo("Project Name"));
+		assertThat(testObj.getEndDate(), IsEqual.equalTo(sampleDate));
+		assertThat(testObj.getProjectId(), IsEqual.equalTo(1));
+		assertThat(testObj.getStartDate(), IsEqual.equalTo(sampleDate));
+		assertThat(testObj.getTasks(), IsEqual.equalTo(null));
+
+	}
+
+}
