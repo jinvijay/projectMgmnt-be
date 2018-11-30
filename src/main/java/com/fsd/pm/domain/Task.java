f5fd762dd27a9d823850025fc947626f0ef124dd
Jinu Vijay <jinu.vijay@gmail.com>
2018-11-29 23:45:42 -0500

    Merged code

--- src/main/java/com/fsd/pm/domain/Task.java
+++ src/main/java/com/fsd/pm/domain/Task.java
@@ -15,10 +15,6 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-import io.swagger.annotations.ApiModelProperty;
-
 @Entity
 @Table(schema = "fsd_pm", name = "task")
 public class Task {
